709bc489db2b69762c81b2d0cd0f66ec90245981
MaykerHM <hydeki8@gmail.com>
2022-06-07 18:22:52 -0300

    feat: ensure all files type is pdf

--- src/presentation/controllers/merge.ts
+++ src/presentation/controllers/merge.ts
@@ -1,9 +1,20 @@
 export class MergeController {
   handle(httpRequest: any): any {
-    if (httpRequest.files.length === 0) {
+    const files = httpRequest.files
+    if (files.length === 0) {
       return {
         statusCode: 400,
         body: new Error('Missing files'),
+      }
+    }
+    const isAllPdfTypeFiles = files.reduce((accumulator, file) => {
+      return accumulator && file.type === 'application/pdf'
+    }, true)
+    if (!isAllPdfTypeFiles) {
+      console.log(files)
+      return {
+        statusCode: 415,
+        body: new Error('Wrong file type'),
       }
     }
   }
