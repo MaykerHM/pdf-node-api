da509c9bb00b920c245a3e3925ff22ac3c835ad7
MaykerHM <hydeki8@gmail.com>
2022-06-07 18:23:06 -0300

    test: ensure all files type is pdf

--- src/presentation/controllers/merge.spec.ts
+++ src/presentation/controllers/merge.spec.ts
@@ -1,4 +1,5 @@
 import { MergeController } from './merge'
+import { Blob } from 'node:buffer'
 
 describe('Merge Controller', () => {
   test('Should return 400 if no files is provided', () => {
@@ -10,5 +11,17 @@ describe('Merge Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing files'))
+  })
+  test('Should return 415 if non-pdf file is provided', () => {
+    const sut = new MergeController()
+    const blob1 = new Blob([''], { type: 'application/text' })
+    const blob2 = new Blob([''], { type: 'application/pdf' })
+    const httpRequest = {
+      files: [blob1, blob2],
+      body: {},
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(415)
+    expect(httpResponse.body).toEqual(new Error('Wrong file type'))
   })
 })
