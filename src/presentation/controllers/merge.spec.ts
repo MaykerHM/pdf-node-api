d0a60e97a623018af579b5cec3eb6f5d6c7d9efc
MaykerHM <hydeki8@gmail.com>
2022-06-14 18:52:09 -0300

    test: ensure MergeController returns 200 if merged pdf is empty

--- src/presentation/controllers/merge.spec.ts
+++ src/presentation/controllers/merge.spec.ts
@@ -2,14 +2,30 @@ import { MergeController } from './merge'
 import { Blob } from 'node:buffer'
 import { MissingFilesError } from '../erros/missing-files-error'
 import { FileTypeError } from '../erros/file-type-error'
+import { PdfEditor } from '../protocols/pdf-editor'
 
-const makeSut = (): MergeController => {
-  return new MergeController()
+interface SutTypes {
+  sut: MergeController
+  pdfEditorStub: PdfEditor
+}
+
+const makeSut = (): SutTypes => {
+  class PdfEditorStub implements PdfEditor {
+    merge(filesPath: string[]): Uint8Array {
+      return new Uint8Array([255])
+    }
+  }
+  const pdfEditorStub = new PdfEditorStub()
+  const sut = new MergeController(pdfEditorStub)
+  return {
+    sut,
+    pdfEditorStub,
+  }
 }
 
 describe('Merge Controller', () => {
   test('Should return 400 if no files is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       files: [],
       body: {},
@@ -19,7 +35,7 @@ describe('Merge Controller', () => {
     expect(httpResponse.body).toEqual(new MissingFilesError())
   })
   test('Should return 415 if non-pdf file is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const blob1 = new Blob([''], { type: 'application/text' })
     const blob2 = new Blob([''], { type: 'application/pdf' })
     const httpRequest = {
@@ -29,5 +45,18 @@ describe('Merge Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(415)
     expect(httpResponse.body).toEqual(new FileTypeError())
+  })
+  test('Should return 200 if merged pdf is empty', () => {
+    const { sut, pdfEditorStub } = makeSut()
+    jest.spyOn(pdfEditorStub, 'merge').mockReturnValueOnce(new Uint8Array([]))
+    const blob1 = new Blob([''], { type: 'application/pdf' })
+    const blob2 = new Blob([''], { type: 'application/pdf' })
+    const httpRequest = {
+      files: [blob1, blob2],
+      body: {},
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual('Empty file')
   })
 })
